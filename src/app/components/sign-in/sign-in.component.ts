4cb6d44515b17aa0db1e19fe6e6577c08285c83e
jsninja777 <topjsdev2017@gmail.com>
2020-09-01 04:06:23 +0800

    Improve UI features

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
-
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
+  faGooglePlusG = faGooglePlusG
   constructor(
     public authService: AuthService
   ) { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA3YKBP_qWcxuVgmAIlbTlND-NPudDLDag",
+    authDomain: "medium-editor-40baf.firebaseapp.com",
+    databaseURL: "https://medium-editor-40baf.firebaseio.com",
+    projectId: "medium-editor-40baf",
+    storageBucket: "medium-editor-40baf.appspot.com",
+    messagingSenderId: "924813414084",
+    appId: "1:924813414084:web:be073b6442308636af0391",
+    measurementId: "G-0YCQ6Q7D8D"
+  }
 };
